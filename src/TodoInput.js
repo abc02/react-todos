583d7d4162ffc7ee2f17c8ddfaac94f0f6f32d82
abc02 <abcqq02@qq.com>
2017-09-17 21:13:19 +0800

    edit todoinput

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 class TodoInput extends Component {
-
+    submit(e){
+        if(e.key === 'Enter'){
+            console.log(e.key)
+        }
+    }
     render() {
         return (
-            <input type="text" defaultValue={this.props.content} />
+            <input type="text" defaultValue={this.props.content} 
+                onKeyPress={this.submit}
+            />
         )
     }
 }
