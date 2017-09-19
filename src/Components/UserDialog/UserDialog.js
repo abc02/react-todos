a1d6b65d566da3599bf0cc2e7ffea5332fa2df53
abc02 <abcqq02@qq.com>
2017-09-19 14:35:41 +0800

    edit SignInOrSignUp

--- src/Components/UserDialog/UserDialog.js
+++ src/Components/UserDialog/UserDialog.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
 import './UserDialog.css'
 import { signUp, signIn, resetPasswordEmail } from '../../ServiceAPI/LeanCloud.js'
-import SignUpForm from './SignUpForm.js'
-import SignInForm from './SignInForm.js'
 import FrgotPassword from './ForgotPasswordForm.js'
+import SignInOrSignUp from './SignInOrSignUp.js'
 
 export default class UserDialog extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            selected: 'signUp',
-            selectedTab:'signInOrSignUp',
+            selectedTab: 'signInOrSignUp',
             formData: {
                 email: '',
                 username: '',
                 password: ''
             }
         }
-        this.switch = this.switch.bind(this)
-        this.resetPasswordEmail = this.resetPasswordEmail.bind(this)
-        this.returnToSignIn = this.returnToSignIn.bind(this)
     }
     showError(error) {
         switch (error.code) {
@@ -34,11 +29,7 @@ export default class UserDialog extends Component {
                 break
         }
     }
-    switch(e) {
-        this.setState({
-            selected: e.target.value
-        })
-    }
+ 
     signUp(e) {
         e.preventDefault()
         let { email, username, password } = this.state.formData
@@ -70,68 +61,38 @@ export default class UserDialog extends Component {
         stateCopy.formData[key] = e.target.value
         this.setState(stateCopy)
     }
-    forgotPassword(){
+    forgotPassword() {
         console.log('forgotPassword')
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.selectedTab = 'forgotPassword'
         this.setState(stateCopy)
     }
-    resetPasswordEmail(e){
+    resetPasswordEmail(e) {
         e.preventDefault()
         resetPasswordEmail(this.state.formData.email)
     }
-    returnToSignIn(){
+    returnToSignIn() {
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.selectedTab = 'signInOrSignUp'
         this.setState(stateCopy)
     }
     render() {
-       
-       
-        let signInOrSignUp = (
-            <div className="signInOrSignUp">
-                <nav>
-                    <label>
-                        <input type="radio"
-                            value="signUp"
-                            checked={this.state.selected === 'signUp'}
-                            onChange={this.switch}
-                        /> 注册
-            </label>
-                    <label>
-                        <input type="radio"
-                            value="signIn"
-                            checked={this.state.selected === 'signIn'}
-                            onChange={this.switch}
-                        /> 登录
-            </label>
-                </nav>
-                <div className="panes">
-                    {this.state.selected === 'signUp' ? <SignUpForm 
-                    formData={this.state.formData}
-                    onSubmit={this.signUp.bind(this)}
-                    onChange={this.changeFormData.bind(this)}
-                    /> : null}
-                    {this.state.selected === 'signIn' ? <SignInForm 
-                    formData={this.state.formData}
-                    onSubmit={this.signUp.bind(this)}
-                    onChange={this.changeFormData.bind(this)}
-                    onForgotPassword={this.forgotPassword.bind(this)}
-                    /> : null}
-                </div>
-            </div>
-        )
-    
         return (
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
-                    {this.state.selectedTab === 'signInOrSignUp' ? signInOrSignUp : 
-                    <FrgotPassword
-                    onResetPasswordEmail={this.resetPasswordEmail.bind(this)}
-                    formData={this.state.formData}
-                    onChange={this.changeFormData.bind(this)}
-                    onReturnToSignIn={this.returnToSignIn.bind(this)}
-                    />
+                    {this.state.selectedTab === 'signInOrSignUp' ?
+                        <SignInOrSignUp
+                            formData={this.state.formData}
+                            onSignUp={this.signUp.bind(this)}
+                            onSignIn={this.signIn.bind(this)}
+                            onChange={this.changeFormData.bind(this)}
+                            onForgotPassword={this.forgotPassword.bind(this)} /> :
+                        <FrgotPassword
+                            onResetPasswordEmail={this.resetPasswordEmail.bind(this)}
+                            formData={this.state.formData}
+                            onChange={this.changeFormData.bind(this)}
+                            onReturnToSignIn={this.returnToSignIn.bind(this)}
+                        />
                     }
                 </div>
 
