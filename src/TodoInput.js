f45ba3345cc01a60deed8ed29f5927b60ddeee8d
abc02 <abcqq02@qq.com>
2017-09-19 14:57:33 +0800

    edit TodoInput -> function

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react'
 import './TodoInput.css'
 
-class TodoInput extends Component {
-    constructor() {
-        // 调用父组件，this指向自己
-        super()
-        this.changeTitle = this.changeTitle.bind(this)
-        this.submit = this.submit.bind(this)
-    }
-    changeTitle(e){
-        // react 自动传入event参数，调用props属性上注册号的onChange方法，传入event参数
-        this.props.onChange(e)
-    }
-    submit(e) {
-        if (e.key === 'Enter') {
-            console.log(e.key)
-            this.props.onSubmit(e)
-        }
-    }
-    render() {
-        return (
-            <input type="text" value={this.props.content}
-                className="TodoInput"
-            // 触发change事件，调用changeTitle方法
-                onChange={this.changeTitle}
-                onKeyPress={this.submit}
-            />
-        )
+function changeTitle(props, e) {
+    // react 自动传入event参数，调用props属性上注册号的onChange方法，传入event参数
+    props.onChange(e)
+}
+function submit(props, e) {
+    if (e.key === 'Enter') {
+        console.log(e.key)
+        props.onSubmit(e)
     }
 }
-
-export default TodoInput
+export default function (props) {
+    return (
+        <input type="text" value={props.content}
+            className="TodoInput"
+            // 触发change事件，调用changeTitle方法
+            onChange={changeTitle.bind(null, props)}
+            onKeyPress={submit.bind(null, props)}
+        />
+    )
+}
