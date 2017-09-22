493dd54d430cce7f55d388bbe4d205ce0bd65a0a
abc02 <abcqq02@qq.com>
2017-09-22 19:19:19 +0800

    edit errorCode

--- src/serviceAPI/errorCode.js
+++ src/serviceAPI/errorCode.js
@@ -1,24 +1,17 @@
-import React from 'react'
-import Error from 'components/error/error.js'
-console.log(Error)
+
 export default function showError(error) {
     console.log(error)
     switch (error.code) {
         case 202:
             return '用户名已被占用';
-            break
         case 203:
             return '该邮箱已被占用';
-            break
         case 210:
             return '用户名或密码错误';
-            break
         case 211:
             return '找不到该用户';
-            break
         default:
             return error.error;
-            break
     }
 }
 
